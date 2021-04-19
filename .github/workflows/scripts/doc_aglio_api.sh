544768eea46ab6cede80c3d2377cc0cae7ea1cc5
Leonardo Mariano <leonardo.h.mariano@hotmail.com>
2021-04-19 11:18:41 -0300

    Update gh-pages script

--- .github/workflows/scripts/doc_aglio_api.sh
+++ .github/workflows/scripts/doc_aglio_api.sh
@@ -2,13 +2,13 @@
 
 mkdir -p ./build-docs
 
-cp $PATH_APIB ./build-docs/api.apib
+cp ./docs/*.apib ./build-docs/
 
 cd ./build-docs
 
 mkdir -p $FOLDER_TO_GH_PAGES
 
-docker run --volume $(pwd):/temp:Z dojot/aglio -i /temp/api.apib -o - > ./$FOLDER_TO_GH_PAGES/apiary_${VERSION_NAME}.html
+docker run --volume $(pwd):/temp:Z dojot/aglio -i /temp/apiary.apib -o - > ./$FOLDER_TO_GH_PAGES/apiary_${VERSION_NAME}.html
 
 rm -rf api.apib
 
